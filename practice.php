db3d0c063363437ea04017f4ad2eab0846cd4c88
keisuke <ookumanote@gmail.com>
2019-12-05 08:24:58 +0000

    （再）HP/Laravel 02 PHPの型と基本的な式を理解しよう

--- practice.php
+++ practice.php
@@ -1,4 +1,32 @@
 <?php
+#課題１
+$a = "3";
+$b = "7";
+echo $a+$b;
+echo "\n";
+?>
+<?php
+#課題２
+$array_month = [1,2,3,4,5,6,7,8,9,10,11,12];
+echo $array_month [7];
+echo "\n";
+?>
+<?php
+#課題３
+$hello = "Hello,";
+$name = "南田";
+$world = "'s World!";
+echo $hello.$name.$world;
+echo "\n";
+?>
+<?php
+#課題４
+$tech_boost = "tech";
+echo $tech_boost." boost";
+echo "\n";
+?>
+<?php
+#課題５
 $calendar_2018 =[
   "January" => "1月",
   "Februry" => "2月",
@@ -14,4 +42,4 @@ $calendar_2018 =[
   "December" => "12月",
   ];
   echo $calendar_2018["December"];
-  
+  ?>
